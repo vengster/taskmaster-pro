5bd81bf50e89680de64afad8b3978e10e13d6847
vengster <veng.lee@gmail.com>
2022-04-22 15:38:38 -0700

    5.1.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,12 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click","p", function(){
+  var text = $(this)
+  .text()
+  .trim();
+  
+})
 
 
 
